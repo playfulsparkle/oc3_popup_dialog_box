af2fc956305d2a410eb86a7f8bb14c4f13296985
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2026-03-29 17:01:04 +0200

    Code optimization and add close dialog label

--- src/upload/catalog/controller/extension/module/ps_popup_dialog_box.php
+++ src/upload/catalog/controller/extension/module/ps_popup_dialog_box.php
@@ -18,64 +18,38 @@ class ControllerExtensionModulePsPopupDialogBox extends Controller
             $this->document->addScript('catalog/view/javascript/ps_popup_dialog_box.min.js');
         }
 
+        $this->load->language('extension/module/ps_popup_dialog_box');
+
         $language_id = $this->config->get('config_language_id');
 
-        $data['content'] = isset($setting['content'][$language_id]) ? html_entity_decode($setting['content'][$language_id], ENT_QUOTES, 'UTF-8') : '';
-
         if ($this->request->server['HTTPS']) {
-            $data['bg_image'] = isset($setting['bg_image'][$language_id]['image']) ? $this->config->get('config_ssl') . 'image/' . html_entity_decode($setting['bg_image'][$language_id]['image'], ENT_QUOTES, 'UTF-8') : '';
+            $image_url = $this->config->get('config_ssl') . 'image/';
         } else {
-            $data['bg_image'] = isset($setting['bg_image'][$language_id]['image']) ? $this->config->get('config_url') . 'image/' . html_entity_decode($setting['bg_image'][$language_id]['image'], ENT_QUOTES, 'UTF-8') : '';
+            $image_url = $this->config->get('config_url') . 'image/';
         }
 
-        switch ($setting['position']) {
-            case 'top_left':
-                $data['position'] = 'top-left';
-                $data['animation_in'] = $setting['animation_in'] . 'TopLeft';
-                $data['animation_out'] = $setting['animation_out'] . 'TopLeft';
-                break;
-            case 'top_center':
-                $data['position'] = 'top-center';
-                $data['animation_in'] = $setting['animation_in'] . 'TopCenter';
-                $data['animation_out'] = $setting['animation_out'] . 'TopCenter';
-                break;
-            case 'top_right':
-                $data['position'] = 'top-right';
-                $data['animation_in'] = $setting['animation_in'] . 'TopRight';
-                $data['animation_out'] = $setting['animation_out'] . 'TopRight';
-                break;
-            case 'center_left':
-                $data['position'] = 'center-left';
-                $data['animation_in'] = $setting['animation_in'] . 'CenterLeft';
-                $data['animation_out'] = $setting['animation_out'] . 'CenterLeft';
-                break;
-            case 'center_right':
-                $data['position'] = 'center-right';
-                $data['animation_in'] = $setting['animation_in'] . 'CenterRight';
-                $data['animation_out'] = $setting['animation_out'] . 'CenterRight';
-                break;
-            case 'bottom_left':
-                $data['position'] = 'bottom-left';
-                $data['animation_in'] = $setting['animation_in'] . 'BottomLeft';
-                $data['animation_out'] = $setting['animation_out'] . 'BottomLeft';
-                break;
-            case 'bottom_center':
-                $data['position'] = 'bottom-center';
-                $data['animation_in'] = $setting['animation_in'] . 'BottomCenter';
-                $data['animation_out'] = $setting['animation_out'] . 'BottomCenter';
-                break;
-            case 'bottom_right':
-                $data['position'] = 'bottom-right';
-                $data['animation_in'] = $setting['animation_in'] . 'BottomRight';
-                $data['animation_out'] = $setting['animation_out'] . 'BottomRight';
-                break;
-            default:
-                // Default to centered if unknown
-                $data['position'] = 'center-center';
-                $data['animation_in'] = $setting['animation_in'] . 'CenterCenter';
-                $data['animation_out'] = $setting['animation_out'] . 'CenterCenter';
-                break;
-        }
+        $data['content'] = isset($setting['content'][$language_id]) ? html_entity_decode($setting['content'][$language_id], ENT_QUOTES, 'UTF-8') : '';
+        $data['bg_image'] = isset($setting['bg_image'][$language_id]['image']) ? $image_url . html_entity_decode($setting['bg_image'][$language_id]['image'], ENT_QUOTES, 'UTF-8') : '';
+
+        $positionMap = array(
+            'top_left' => array('position' => 'top-left', 'suffix' => 'TopLeft'),
+            'top_center' => array('position' => 'top-center', 'suffix' => 'TopCenter'),
+            'top_right' => array('position' => 'top-right', 'suffix' => 'TopRight'),
+            'center_left' => array('position' => 'center-left', 'suffix' => 'CenterLeft'),
+            'center_center' => array('position' => 'center-center', 'suffix' => 'CenterCenter'),
+            'center_right' => array('position' => 'center-right', 'suffix' => 'CenterRight'),
+            'bottom_left' => array('position' => 'bottom-left', 'suffix' => 'BottomLeft'),
+            'bottom_center' => array('position' => 'bottom-center', 'suffix' => 'BottomCenter'),
+            'bottom_right' => array('position' => 'bottom-right', 'suffix' => 'BottomRight'),
+        );
+
+        $pos = isset($positionMap[$setting['position']])
+            ? $positionMap[$setting['position']]
+            : array('position' => 'center-center', 'suffix' => 'CenterCenter');
+
+        $data['position'] = $pos['position'];
+        $data['animation_in'] = $setting['animation_in'] . $pos['suffix'];
+        $data['animation_out'] = $setting['animation_out'] . $pos['suffix'];
 
         $data['trigger'] = $setting['trigger'];
         $data['close_behavior'] = $this->getCookieDays($setting['close_behavior']);
